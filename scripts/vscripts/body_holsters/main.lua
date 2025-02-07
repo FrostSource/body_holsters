5bc224798c573fd6aa7a499f5a65ea59c995cdf8
FrostSource <jjhfrost@gmail.com>
2025-02-08 09:04:34 +1300

    Add custom button convars

--- scripts/vscripts/body_holsters/main.lua
+++ scripts/vscripts/body_holsters/main.lua
@@ -6,53 +6,242 @@ local function convarUpdateController()
     BodyHolsters:UpdateControllerInputs()
 end
 
+---Used to test other controller value types without actually having the controller
+local function getVRControllerType()
+    if IsInToolsMode() then
+        -- return VR_CONTROLLER_TYPE_KNUCKLES
+        -- return VR_CONTROLLER_TYPE_RIFT_S
+        -- return VR_CONTROLLER_TYPE_VIVE
+        return Player:GetVRControllerType()
+    else
+        return Player:GetVRControllerType()
+    end
+end
+
 EasyConvars:RegisterConvar("body_holsters_visible_weapons", "0", "Weapons are visibly attached to the player body.", 0)
 EasyConvars:SetPersistent("body_holsters_visible_weapons", true)
 
-EasyConvars:RegisterConvar("body_holsters_increase_offhand_side_radius", "1", "Slots on the side of the body opposite to the primary hand are increased.", 0)
+EasyConvars:RegisterConvar("body_holsters_increase_offhand_side_radius", "1", "Body slots on the non-dominant side of your body will have a slightly larger radius to accommodate for increased reach distance.", 0)
 EasyConvars:SetPersistent("body_holsters_increase_offhand_side_radius", true)
 
 EasyConvars:RegisterConvar("body_holsters_allow_multitool", "0", "Multitool is allowed to be holstered.", 0)
 EasyConvars:SetPersistent("body_holsters_allow_multitool", true)
 
-EasyConvars:RegisterConvar("body_holsters_unholster_grip_amount", function()
-    if Player:GetVRControllerType() == VR_CONTROLLER_TYPE_KNUCKLES and EasyConvars:GetBool("body_holsters_knuckles_use_squeeze") then
-        return 0.5
-    else
-        return 1.0
-    end
-end, "[0-1] value for how much the controller must be squeezed to unholster", 0, convarUpdateController)
+EasyConvars:RegisterConvar("body_holsters_unholster_grip_amount", 1.0, "[0-1] value for how much the controller must be gripped to unholster a weapon (only applicable if body_holsters_unholster_is_analog is 1)", 0, convarUpdateController)
 EasyConvars:SetPersistent("body_holsters_unholster_grip_amount", true)
 
-EasyConvars:RegisterConvar("body_holsters_holster_ungrip_amount", 0.1, "[0-1] value for how much the controller must be ungripped to holster", 0, convarUpdateController)
+EasyConvars:RegisterConvar("body_holsters_holster_ungrip_amount", 0.1, "[0-1] value for how much the controller must be ungripped to holster a weapon (only applicable if body_holsters_holster_is_analog is 1)", 0, convarUpdateController)
 EasyConvars:SetPersistent("body_holsters_holster_ungrip_amount", true)
 
-EasyConvars:RegisterConvar("body_holsters_require_trigger_to_unholster", "0", "Trigger button (fire) must be pressed to unholster a weapon.", 0, convarUpdateController)
-EasyConvars:SetPersistent("body_holsters_require_trigger_to_unholster", true)
 
-EasyConvars:RegisterConvar("body_holsters_knuckles_use_squeeze", "1", "Knuckles controllers will use the xen squeeze analog instead of hand curl", 0, function (newVal, oldVal)
-    -- Automatically update the grip amount if the user hasn't set a custom value
-    if not EasyConvars:GetBool("body_holsters_knuckles_use_squeeze") and not EasyConvars:WasChangedByUser("body_holsters_unholster_grip_amount") then
-        EasyConvars:SetFloat("body_holsters_unholster_grip_amount", 1.0)
-    end
-end)
-EasyConvars:SetPersistent("body_holsters_knuckles_use_squeeze", true)
+-- EasyConvars:RegisterConvar("body_holsters_require_trigger_to_unholster", "0", "Trigger button (shoot) must be pressed to unholster a weapon instead of the grip.", 0, function (newVal, oldVal)
+--     if truthy(newVal) then
+--         EasyConvars:SetBool("body_holsters_unholster_is_analog", true)
+--         EasyConvars:SetInt("body_holsters_unholster_action", DIGITAL_INPUT_FIRE)
+
+--         EasyConvars:SetWasChanged("body_holsters_unholster_is_analog", true)
+--         EasyConvars:SetWasChanged("body_holsters_unholster_action", true)
+--     end
+
+--     convarUpdateController()
+-- end)
+-- EasyConvars:SetPersistent("body_holsters_require_trigger_to_unholster", true)
+
+
+-- EasyConvars:RegisterConvar("body_holsters_knuckles_use_squeeze", "1", "Knuckles controllers will use the xen squeeze analog instead of hand curl", 0, function (newVal, oldVal)
+--     -- Automatically update the grip amount if the user hasn't set a custom value
+--     if not EasyConvars:GetBool("body_holsters_knuckles_use_squeeze") and not EasyConvars:WasChangedByUser("body_holsters_unholster_grip_amount") then
+--         EasyConvars:SetFloat("body_holsters_unholster_grip_amount", 1.0)
+--     end
+-- end)
+-- EasyConvars:SetPersistent("body_holsters_knuckles_use_squeeze", true)
+
 
 EasyConvars:RegisterConvar("body_holsters_use_procedural_angles", "0", "Visible weapons will use the angle of the weapon when holstered.", 0)
 EasyConvars:SetPersistent("body_holsters_use_procedural_angles", true)
+
+
+EasyConvars:RegisterConvar("body_holsters_holster_is_analog", function()
+    -- Vive doesn't have analog grip for grabbing apparently, so it will use button above touchpad (slide release)
+    if getVRControllerType() == VR_CONTROLLER_TYPE_VIVE then
+        return false
+    else
+        return true
+    end
+end, "Whether analog actions should be used instead of digital actions for holstering.", 0, nil,
+-- Display func
+function (reg)
+    Msg(reg.name .. " = " .. tostring(reg.value) .. "\n")
+    Msg("Whether analog actions should be used instead of digital actions for holstering. ")
+    if truthy(reg.value) then
+        Msg("Holstering is using analog actions.\n")
+    else
+        Msg("Holstering is using digital actions.\n")
+    end
+end)
+EasyConvars:SetPersistent("body_holsters_holster_is_analog", true)
+
+
+EasyConvars:RegisterConvar("body_holsters_holster_action", ANALOG_INPUT_HAND_CURL, "The digital or analog action for holstering.", 0, convarUpdateController,
+-- Display func
+function (reg)
+    Msg(reg.name .. " = " .. tostring(reg.value) .. "\n")
+    if EasyConvars:GetBool("body_holsters_holster_is_analog") then
+        Msg("Holster analog action is " .. Input:GetAnalogDescription(tonumber(reg.value)) .. "\n")
+    else
+        Msg("Holster digital action is " .. Input:GetButtonDescription(tonumber(reg.value)) .. "\n")
+    end
+end)
+EasyConvars:SetPersistent("body_holsters_holster_action", true)
+
+
+EasyConvars:RegisterConvar("body_holsters_unholster_is_analog", true, "Whether analog actions should be used instead of digital actions for unholstering", 0, nil,
+-- Display func
+function (reg)
+    Msg(reg.name .. " = " .. tostring(reg.value) .. "\n")
+    Msg("Whether analog actions should be used instead of digital actions for unholstering. ")
+    if truthy(reg.value) then
+        Msg("Unholstering is using analog actions.\n")
+    else
+        Msg("Unholstering is using digital actions.\n")
+    end
+end)
+EasyConvars:SetPersistent("body_holsters_unholster_is_analog", true)
+
+
+EasyConvars:RegisterConvar("body_holsters_unholster_action", function()
+    if getVRControllerType() == VR_CONTROLLER_TYPE_KNUCKLES then
+        return ANALOG_INPUT_SQUEEZE_XEN_GRENADE
+    else
+        return ANALOG_INPUT_HAND_CURL
+    end
+end, "The digital or analog action for unholstering", 0, convarUpdateController,
+-- Display func
+function (reg)
+    Msg(reg.name .. " = " .. tostring(reg.value) .. "\n")
+    if EasyConvars:GetBool("body_holsters_unholster_is_analog") then
+        Msg("Unholster analog action is " .. Input:GetAnalogDescription(tonumber(reg.value)) .. "\n")
+    else
+        Msg("Unholster digital action is " .. Input:GetButtonDescription(tonumber(reg.value)) .. "\n")
+    end
+end)
+EasyConvars:SetPersistent("body_holsters_unholster_action", true)
+
+
+---All convar work that needs to be done after convars have finished setting up
+---Extra checks need to happen here to make sure we don't overwrite user's custom values
+EasyConvars:SetPostInitializer(function()
+
+    local controllerType = getVRControllerType()
+    print("CONTROLLER TYPE: " .. controllerType, Input:GetControllerTypeDescription(controllerType))
+
+    -- Automatic holster actions
+    if not EasyConvars:WasChangedByUser("body_holsters_holster_action") then
+        if EasyConvars:GetBool("body_holsters_holster_is_analog") then
+            EasyConvars:SetInt("body_holsters_holster_action", ANALOG_INPUT_HAND_CURL)
+        else
+            -- Vive doesn't have analog grip for grabbing apparently, so it will use button above touchpad (slide release)
+            if controllerType == VR_CONTROLLER_TYPE_VIVE then
+                EasyConvars:SetInt("body_holsters_holster_action", DIGITAL_INPUT_SLIDE_RELEASE)
+            else
+                EasyConvars:SetInt("body_holsters_holster_action", DIGITAL_INPUT_USE_GRIP)
+            end
+        end
+    end
+
+    -- Automatic unholster actions
+    if not EasyConvars:WasChangedByUser("body_holsters_unholster_action") then
+        if EasyConvars:GetBool("body_holsters_unholster_is_analog") then
+            -- Index automatically gets squeeze analog
+            if controllerType == VR_CONTROLLER_TYPE_KNUCKLES then
+                EasyConvars:SetInt("body_holsters_unholster_action", ANALOG_INPUT_SQUEEZE_XEN_GRENADE)
+                EasyConvars:SetIfUnchanged("body_holsters_unholster_grip_amount", 0.5)
+            else
+                EasyConvars:SetInt("body_holsters_unholster_action", ANALOG_INPUT_HAND_CURL)
+            end
+        else
+            EasyConvars:SetInt("body_holsters_unholster_action", DIGITAL_INPUT_USE_GRIP)
+        end
+    end
+
+end)
 
 require "alyxlib.controls.input"
 Input.AutoStart = true
 
 local cloneName = "__weapon_clone"
 
-local version = "v1.0.0"
+local version = "v1.1.0"
 
 ---@class BodyHolsters
 BodyHolsters = {}
 BodyHolsters.version = version
 BodyHolsters.__index = BodyHolsters
 
+local addonID = RegisterAlyxLibAddon("Body Holsters", version, "3144612716", "body_holsters", "v1.3.0", nil)
+
+local lastThinkTime = 0
+
+RegisterAlyxLibDiagnostic(addonID, function ()
+    if not Player.HMDAvatar then
+        return false, "Resin watch requires VR or +vr_enable_fake_vr to be enabled"
+    end
+
+    local convars = {
+        "body_holsters_visible_weapons",
+        "body_holsters_increase_offhand_side_radius",
+        "body_holsters_allow_multitool",
+        "body_holsters_unholster_grip_amount",
+        "body_holsters_holster_ungrip_amount",
+        "body_holsters_use_procedural_angles",
+        "body_holsters_holster_is_analog",
+        "body_holsters_holster_action",
+        "body_holsters_unholster_is_analog",
+        "body_holsters_unholster_action",
+    }
+
+    for _, convar in ipairs(convars) do
+        if EasyConvars:WasChangedByUser(convar) then
+            Msg(convar .. " = " .. EasyConvars:GetStr(convar) .. "\n")
+        end
+    end
+
+    local holsterIsAnalog = EasyConvars:GetBool("body_holsters_holster_is_analog")
+    local holsterAction = EasyConvars:GetFloat("body_holsters_holster_action")
+    if holsterIsAnalog then
+        if holsterAction  < 0 or holsterAction > 4 then
+            return false, "body_holsters_holster_action must be an analog action ranging from 0 to 4. See this guide for more info: "
+        end
+    else
+        if holsterAction < 0 or holsterAction > 27 then
+            return false, "body_holsters_holster_action must be a digital action ranging from 0 to 27. See this guide for more info: "
+        end
+    end
+
+    local unholsterIsAnalog = EasyConvars:GetBool("body_holsters_unholster_is_analog")
+    local unholsterAction = EasyConvars:GetFloat("body_holsters_unholster_action")
+    if unholsterIsAnalog then
+        if unholsterAction  < 0 or unholsterAction > 4 then
+            return false, "body_holsters_unholster_action must be an analog action ranging from 0 to 4. See this guide for more info: "
+        end
+    else
+        if unholsterAction < 0 or unholsterAction > 27 then
+            return false, "body_holsters_unholster_action must be a digital action ranging from 0 to 27. See this guide for more info: "
+        end
+    end
+
+    if not Player then
+        return false, "Player entity not found."
+    end
+
+    if not Player:GetBackpack() then
+        return false, "Backpack entity not found. Backpack is required for body holsters to function correctly."
+    end
+
+    Msg("Body Holsters last think time: " .. Time() - lastThinkTime .. " seconds ago (" .. lastThinkTime .. ")\n")
+
+    return true
+end)
 
 ---When you look down your head origin moves forwards but body and arms stay the same place
 ---This causes a perceived disparity between where the slots actually are vs where you think they should be
@@ -489,15 +678,23 @@ local inputUnholsterID
 ---Updates the input callbacks using the current settings.
 function BodyHolsters:UpdateControllerInputs()
     Input:StopListening(inputUnholsterID)
-    if EasyConvars:GetBool("body_holsters_require_trigger_to_unholster") then
-        inputUnholsterID = Input:ListenToButton("press", 2, DIGITAL_INPUT_FIRE, 1, inputUnholsterCallback)
+    if EasyConvars:GetBool("body_holsters_unholster_is_analog") then
+        print("Unholster analog")
+        inputUnholsterID = Input:ListenToAnalog("up", 2, EasyConvars:GetInt("body_holsters_unholster_action"), EasyConvars:GetFloat("body_holsters_unholster_grip_amount"), inputUnholsterCallback)
     else
-        local analogAction = vlua.select(EasyConvars:GetBool("body_holsters_knuckles_use_squeeze"), ANALOG_INPUT_SQUEEZE_XEN_GRENADE, ANALOG_INPUT_HAND_CURL)
-        inputUnholsterID = Input:ListenToAnalog("up", 2, analogAction, EasyConvars:GetFloat("body_holsters_unholster_grip_amount"), inputUnholsterCallback)
+        print("Unholster button")
+        inputUnholsterID = Input:ListenToButton("press", 2, EasyConvars:GetInt("body_holsters_unholster_action"), 1, inputUnholsterCallback)
     end
 
     Input:StopListening(inputHolsterID)
-    inputHolsterID = Input:ListenToAnalog("down", 2, ANALOG_INPUT_HAND_CURL, EasyConvars:GetFloat("body_holsters_holster_ungrip_amount"), inputHolsterCallback)
+    -- inputHolsterID = Input:ListenToAnalog("down", 2, ANALOG_INPUT_HAND_CURL, EasyConvars:GetFloat("body_holsters_holster_ungrip_amount"), inputHolsterCallback)
+    if EasyConvars:GetBool("body_holsters_holster_is_analog") then
+        print("Holster analog")
+        inputHolsterID = Input:ListenToAnalog("down", 2, EasyConvars:GetInt("body_holsters_holster_action"), EasyConvars:GetFloat("body_holsters_holster_ungrip_amount"), inputHolsterCallback)
+    else
+        print("Holster button")
+        inputHolsterID = Input:ListenToButton("press", 2, EasyConvars:GetInt("body_holsters_holster_action"), 1, inputHolsterCallback)
+    end
 end
 
 local handWithinSlot = false
